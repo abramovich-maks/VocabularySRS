0bed3f40e128813864fb3cba8bb2785a7884e68e
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-12-11 19:10:15 +0100

    Initial commit

---
+++ src/main/java/com/vocabularysrs/VocabularySrsApplication.java
@@ -0,0 +1,13 @@
+package com.vocabularysrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularySrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabularySrsApplication.class, args);
+    }
+
+}
