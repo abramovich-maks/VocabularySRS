244c1eef126a640bf7a1d9d75d584723d448e767
abramovich-maks <maks.abramovich.1995@mail.ru>
2026-01-16 14:07:36 +0100

    Improve exception message.

--- src/main/java/com/vocabularysrs/domain/worddetails/WordNotFoundInDictionaryException.java
+++ src/main/java/com/vocabularysrs/domain/worddetails/WordNotFoundInDictionaryException.java
@@ -3,6 +3,6 @@ package com.vocabularysrs.domain.worddetails;
 public class WordNotFoundInDictionaryException extends RuntimeException {
 
     public WordNotFoundInDictionaryException(String word) {
-        super("Word not found in dictionary: " + word);
+        super("Unknown word: «" + word + "». Check for typos or enter manually.");
     }
 }
